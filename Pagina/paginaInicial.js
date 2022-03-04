32d0ba83d70218202d55ce359b596ce4de7e4868
mttcordero <65349656+mttcordero@users.noreply.github.com>
2022-03-04 22:00:09 +0000

    PAP

---
+++ Pagina/paginaInicial.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () =>{
+
+
+    document.getElementById("login").onclick = function () {
+        location.href = "login.html";
+    };
+
+    document.getElementById("Navegar").onclick = function () {
+        location.href = "produtos.html";
+    };
+  
+
+});
